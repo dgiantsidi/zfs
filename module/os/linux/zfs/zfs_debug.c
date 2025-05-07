98c00b698fe0187cc9844674e30dd9bc66987d22
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-05-07 13:24:36 +0000

    thread id when logging the debug msg

--- module/os/linux/zfs/zfs_debug.c
+++ module/os/linux/zfs/zfs_debug.c
@@ -169,8 +169,8 @@ __dprintf(boolean_t dprint, const char *file, const char *func,
 		newfile = file;
 	}
 
-	i = snprintf(buf, size, "%px %s%s:%d:%s(): ",
-	    curthread, prefix, newfile, line, func);
+	i = snprintf(buf, size, "tid(%d):%s%s:%d:%s(): ",\
+		current->pid, prefix, newfile, line, func);
 
 	if (i < size) {
 		va_start(adx, fmt);
